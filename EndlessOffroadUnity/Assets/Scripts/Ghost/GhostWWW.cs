e84da3f5ce8b1125c2c47bc76c0246189bdeff33
djmorrsee <djmorrsee@gmail.com>
2015-06-16 22:24:26 -0600

    Networking Code
    Started making the endpoints for the network scripts (high scores,
    ghosts, etc).
    
    Delegates are a lot easier to use than I remember.

---
+++ EndlessOffroadUnity/Assets/Scripts/Ghost/GhostWWW.cs
@@ -0,0 +1,36 @@
+﻿// Script created by Daniel Morrissey
+// Copyrights may apply, see project LICENSE
+//
+// If changes are made to this code, please email djmorrsee@gmail.com
+
+using UnityEngine;
+using System.Collections;
+
+public class GhostWWW : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PostGhostForSeed(new GhostController.GhostData(), 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PostGhostForSeed(GhostController.GhostData ghost, int seed)
+    {
+        StartCoroutine(WWW_Controller.PostHighScoreForSeed(10, 10, GetGhostCallback));
+    }
+
+    void GetGhostsForSeed(int seed)
+    {
+        
+    }
+
+    void GetGhostCallback(string message)
+    {
+        print(message);
+        StartCoroutine(WWW_Controller.PostHighScoreForSeed(10, 10, GetGhostCallback));
+    }
+}
---
+++ EndlessOffroadUnity/Assets/Scripts/Network/WWW_Controller.cs
@@ -0,0 +1,41 @@
+﻿// Script created by Daniel Morrissey
+// Copyrights may apply, see project LICENSE
+//
+// If changes are made to this code, please email djmorrsee@gmail.com
+
+using UnityEngine;
+using System.Collections;
+
+public class WWW_Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public delegate void WWW_Callback(string message);
+    public static IEnumerator PostHighScoreForSeed(int score, int seed, WWW_Callback callback)
+    {
+        string test = "test";
+        byte[] testb = System.Text.Encoding.Unicode.GetBytes(test);
+        WWW post = new WWW("10.0.0.10:5555", testb);
+        yield return new WaitForSeconds(1.0f);
+        yield return post;
+        if(post.error != null)
+        {
+            callback(post.error);
+        } else
+        {
+            callback(post.text);
+        }
+    }
+
+    public static IEnumerator GetHighScoresForSeed(int score)
+    {
+        yield return null;
+    }
+}
